c4e34344f3a3f886ccd8aff827dd9f1a02c86732
@cawfree <cawfree@gmail.com>
2019-06-16 01:04:30 +0100

    introduced easing function

--- example/src/TensionSheet.js
+++ example/src/TensionSheet.js
@@ -9,23 +9,57 @@ class TensionSheet extends React.Component {
     super(nextProps);
     this.__onLayout = this.__onLayout.bind(this);
     this.state = {
-
+      animValue: new Animated.Value(0),
     };
   }
   __onLayout(e) { /* android hack */ }
   componentDidMount() {
-    const { onOnScroll } = this.props;
+    const {
+      onOnScroll,
+      maxHeight,
+      tension,
+    } = this.props;
+    const incr = (1 / maxHeight);
     const { footer } = this.refs;
     onOnScroll((e) => {
-      footer._component.measure(console.log);
+      const {
+        contentOffset: {
+          y,
+        },
+        contentSize: {
+          height: totalContentHeight,
+        },
+        layoutMeasurement: {
+          height: layoutHeight,
+        },
+      } = e.nativeEvent;
+      footer._component.measure(
+        (ox, oy, width, height, px, py) => {
+          const { animValue } = this.state;
+          const barHeight = animValue.__getValue() * maxHeight;
+          const contentHeight = totalContentHeight - barHeight;
+          const scrollLimit = contentHeight - layoutHeight;
+          // XXX: for the maximum scroll before viewing the tensionsheet
+          if (y >= scrollLimit) {
+            animValue.setValue(
+              Math.min(
+                1,
+                tension(((y - scrollLimit) + 1) * incr),
+              ),
+            );
+          } else {
+            animValue.setValue(0);
+          }
+        },
+      );
     });
   }
   render() {
     const {
-
+      maxHeight,
     } = this.props;
     const {
-
+      animValue,
     } = this.state;
     return (
       <Animated.View
@@ -36,7 +70,10 @@ class TensionSheet extends React.Component {
         onLayout={this.__onLayout}
         style={{
           width: 100,
-          height: 100,
+          height: Animated.multiply(
+            animValue,
+            maxHeight,
+          ),
           backgroundColor: 'orange',
         }}
       >
@@ -47,10 +84,16 @@ class TensionSheet extends React.Component {
 
 TensionSheet.propTypes = {
   onOnScroll: PropTypes.func,
+  maxHeight: PropTypes.number,
+  tension: PropTypes.func,
 };
 
 TensionSheet.defaultProps = {
   onOnScroll: onScroll => null,
+  maxHeight: 100,
+  tension: (t) => {
+    return 1+(--t)*t*t*t*t ;
+  },
 };
 
 export default TensionSheet;
